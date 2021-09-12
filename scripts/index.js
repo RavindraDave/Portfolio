09981a9fdd16843b128260c0f0cbed2b3293e586
RavindraDave <37210832+RavindraDave@users.noreply.github.com>
2021-09-12 22:45:27 +0800

    Commit JS Change

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,12 @@
     "use strict";
     
     // Typed Initiate
-        var typed = new Typed('.typed-text', {
-            strings: ["Full Stack Developer","Business Analyst","System Analyst","Tech Enthusiast"],
-            typeSpeed: 120,
-            backSpeed: 30,
-            smartBackspace: true,
-            loop: true
-        });
+    var typed = new Typed('.typed-text', {
+        strings: ["Full Stack Developer","Business Analyst","System Analyst","Tech Enthusiast"],
+        typeSpeed: 120,
+        backSpeed: 30,
+        smartBackspace: true,
+        loop: true
+    });
     
 })(jQuery);
