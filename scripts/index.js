e0d3678db5acac664f44fa1bb5f3efbda34ecd8c
RavindraDave <37210832+RavindraDave@users.noreply.github.com>
2021-09-12 22:33:10 +0800

    Commit JS

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+(function ($) {
+    "use strict";
+    
+    // Typed Initiate
+        var typed = new Typed('.typed-text', {
+            strings: ["Full Stack Developer","Business Analyst","System Analyst","Tech Enthusiast"],
+            typeSpeed: 150,
+            backSpeed: 30,
+            smartBackspace: true,
+            loop: true
+        });
+    
+})(jQuery);
