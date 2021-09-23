391c90398f77f0b041f0ca57ea90e878353c060e
RavindraDave <37210832+RavindraDave@users.noreply.github.com>
2021-09-23 19:45:37 +0800

    Navigation bar and projects img formatting.

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,29 @@
-(function ($) {
-    "use strict";
+$(document).ready(function (){
     
+        $(".topnav a").click(function (event){
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 20
+            }, 1000);
+
+            $('.active').removeClass('active');
+            $(this).closest('a').addClass('active');
+
+        });
+
     // Typed Initiate
-    var typed = new Typed('.typed-text', {
+    let typed = new Typed('.typed-text', {
         strings: ["Full Stack Developer","Business Analyst","System Analyst","Tech Enthusiast"],
         typeSpeed: 120,
         backSpeed: 30,
         smartBackspace: true,
         loop: true
     });
+
+
+
+});
     
-})(jQuery);
+
+
